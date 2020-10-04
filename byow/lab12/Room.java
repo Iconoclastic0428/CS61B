5deb5749d9b8ddb05403122b566ae888fbab02fd
Iconoclastic0428 <69107884+Iconoclastic0428@users.noreply.github.com>
2020-10-04 21:12:16 +0800

    Create Room.java

---
+++ byow/lab12/Room.java
@@ -0,0 +1,12 @@
+package byow.lab12;
+
+public class Room {
+    public int x, y, width, height;
+
+    public Room(int x, int y, int width, int height) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
+}
